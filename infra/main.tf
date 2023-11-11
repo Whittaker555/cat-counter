351062008c050e970fa79de3becb603681d77dd9
george.whittaker <george.whittaker@awaze.com>
2023-11-11 21:48:47 +0000

    working infra

--- infra/main.tf
+++ infra/main.tf
@@ -66,11 +66,35 @@ resource "aws_s3_bucket_ownership_controls" "website_ownership" {
     object_ownership = "BucketOwnerPreferred"
   }
 }
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.website_ownership]
 
+resource "aws_s3_bucket_public_access_block" "website_bucket_access_block" {
   bucket = aws_s3_bucket.website_bucket.id
-  acl    = "private"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website_bucket" {
+    bucket = aws_s3_bucket.website_bucket.id
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "${aws_s3_bucket.website_bucket.arn}",
+          "${aws_s3_bucket.website_bucket.arn}/*"
+        ]
+      },
+    ]
+  })
 }
 
 # LAMBDA
