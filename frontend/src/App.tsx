94fbf95593e878afdeb83980877406f9038e6d20
george.whittaker <george.whittaker@awaze.com>
2023-11-11 20:56:38 +0000

    again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { ConfigProvider} from 'antd';
 import React from 'react';
-import MyButton from './components/mybutton';
 import MyApp from './MyApp';
 
 const App: React.FC = () => (
--- frontend/src/MyApp.tsx
+++ frontend/src/MyApp.tsx
@@ -1,17 +1,17 @@
 import { Button } from 'antd';
-import React, { useState } from "react";
+import React from "react";
 import Popup from "./components/popup";
 
 function MyApp() {
 
-    const [clicked, setClicked] = React.useState<number>(0); // default is 'middle'
+    const [clicked, setClicked] = React.useState<number>(0);
 
         return (
                 <div className="App">
                 <Button type="primary" onClick={() => setClicked(clicked + 1)}>ForCat</Button>
                 {
                         Array.from({ length: clicked }, (_, i) => {
-                            if (i == 5) {
+                            if (i === 5) {
                                 return <h1>I LOVE YOU!</h1>
                             }
                             return <Popup key={i} />
